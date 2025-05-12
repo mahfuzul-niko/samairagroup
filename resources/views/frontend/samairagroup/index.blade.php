ecdbc5214896cff86a7b4faf408805fcd7334534
sadiahaque00 <zenkxeharry@gmail.com>
2025-05-12 15:16:48 +0600

    new footer

--- resources/views/frontend/samairagroup/index.blade.php
+++ resources/views/frontend/samairagroup/index.blade.php
@@ -23,17 +23,9 @@
 <body>
 
      <!-- Loader -->
-  <div id="loader">
+  <div id="loader" style="background:url({{ asset('assets/frontassets/') }}/images/loding-page.jpg) no-repeat center center; background-size: cover;">
     <div class="animated-text font-weight-bold text-uppercase">
-      <!-- Each letter wrapped in a span -->
-       <img src="{{ asset('assets/frontassets/') }}/images/loader-logo.png" alt="Samaira Group Logo" class="img-fluid">
-      <!-- <span>W</span><span>E</span><span>L</span><span>C</span><span>o</span><span>m</span><span>e</span>
-      &nbsp;
-      <span>t</span><span>o</span>
-      &nbsp;
-      <span>S</span><span>a</span><span>m</span><span>a</span><span>i</span><span>r</span><span>a</span>
-      &nbsp;
-      <span>G</span><span>r</span><span>o</span><span>u</span><span>p</span> -->
+       <!-- <img src="assets/images/loding-page.jpg" alt="Samaira Group Logo" class="img-fluid"> -->
     </div>
   </div>
 
@@ -263,97 +255,57 @@
   </section>
   <!-- Subscribe Section End-->
 
-  <!-- Logo and Description Column Start-->
-   <div class="logo-description py-5">
+  <!-- Custom Footer Section Start -->
+  <footer class="cg-footer-section py-4">
     <div class="container">
-      <div class="row align-items-center">
-        <div class="col-lg-6 col-md-6 text-center text-lg-start mb-4 mb-lg-0">
-          <div class="footer-logo">
-            <img src="{{ asset('assets/frontassets/') }}/images/loader-logo.png" alt="Samaira Group Logo" class="img-fluid">
-          </div>
-        </div>
-        <div class="col-lg-2 col-md-6 text-center text-lg-start mb-3 mb-lg-0">
-          <h5 class="follow-title mb-0">Follow Us</h5>
-        </div>
-        <div class="col-lg-4 col-md-12 text-center text-lg-start">
-          <div class="social-links">
-            <a href="#" class="social-link"><i class="fab fa-facebook-f"></i></a>
-            <a href="#" class="social-link"><i class="fab fa-twitter"></i></a>
-            <a href="#" class="social-link"><i class="fab fa-instagram"></i></a>
-            <a href="#" class="social-link"><i class="fab fa-linkedin-in"></i></a>
-          </div>
+      <div class="row align-items-center py-4">
+        <div class="col-12 d-flex align-items-center justify-content-center gap-3">
+          <div class="flex-grow-1 border-top border-info"></div>
+          <img src="{{ asset('assets/frontassets/') }}/images/logo1.png" alt="Samaira Group Logo" class="cg-footer-main-logo">
+          <div class="flex-grow-1 border-top border-info"></div>
         </div>
       </div>
-    </div>
-  </div>
-  <!-- Logo and Description Column End-->
-
-  <!-- Footer Section Start -->
-  <footer class="footer-section mt-5 pt-5">
-    <div class="container">
-      <div class="row">
-
-        <!-- Discover Column -->
-        <div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
-          <h4 class="mb-4">Discover</h4>
-          <ul class="list-unstyled">
-            <li class="mb-2"><a href="#" class="text-decoration-none text-muted">Miami</a></li>
-            <li class="mb-2"><a href="#" class="text-decoration-none text-muted">New York</a></li>
-            <li class="mb-2"><a href="#" class="text-decoration-none text-muted">Chicago</a></li>
-            <li class="mb-2"><a href="#" class="text-decoration-none text-muted">Florida</a></li>
-            <li class="mb-2"><a href="#" class="text-decoration-none text-muted">Los Angeles</a></li>
-            <li class="mb-2"><a href="#" class="text-decoration-none text-muted">San Diego</a></li>
+      <div class="row py-4">
+        <div class="col-md-2 mb-4 mb-md-0">
+          <h3 class="text-info fw-bold border-bottom pb-2 mb-3">USEFUL LINKS</h3>
+          <ul class="list-unstyled cg-footer-section-ul">
+            <li><a href="#" class="text-decoration-none">About Us</a></li>
+            <li><a href="#" class="text-decoration-none">Our Management</a></li>
+            <li><a href="#" class="text-decoration-none">Sister Concern</a></li>
+            <li><a href="#" class="text-decoration-none">Search for Job</a></li>
           </ul>
         </div>
-
-        <!-- Quick Links Column -->
-        <div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
-          <h4 class="mb-4">Quick Links</h4>
-          <ul class="list-unstyled">
-            <li class="mb-2"><a href="#" class="text-decoration-none text-muted">About</a></li>
-            <li class="mb-2"><a href="#" class="text-decoration-none text-muted">Contact</a></li>
-            <li class="mb-2"><a href="#" class="text-decoration-none text-muted">FAQ's</a></li>
-            <li class="mb-2"><a href="#" class="text-decoration-none text-muted">Blog</a></li>
-            <li class="mb-2"><a href="#" class="text-decoration-none text-muted">Pricing Plans</a></li>
-            <li class="mb-2"><a href="#" class="text-decoration-none text-muted">Privacy Policy</a></li>
-            <li class="mb-2"><a href="#" class="text-decoration-none text-muted">Terms & Conditions</a></li>
-          </ul>
+        <div class="col-md-7 mb-4 mb-md-0">
+          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3652.278495061193!2d90.4070121747916!3d23.737446289274466!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3755b9183ec90c79%3A0xe1511972be9e4337!2sSamaira%20Skill%20Development%20Institute%20(SSDI)!5e0!3m2!1sen!2sbd!4v1747031766910!5m2!1sen!2sbd" width="750" height="250" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>        </div>
+        <div class="col-md-3">
+          <h3 class="text-info fw-bold border-bottom pb-2 mb-3">CONTACT US</h3>
+          <address class="text-info">
+            Navana Obaid Eternia, 3rd Floor
+            28/A-29/B Kakrail, Dhaka 1217
+            Bangladesh<br><br>
+            Navana Obaid Eternia, 3rd Floor
+            28/A-29/B Kakrail, Dhaka 1217
+            Bangladesh<br><br>
+            <a href="tel:+8801313770460" class="text-info text-decoration-underline">Phone:</a><br>
+            <a href="tel:+8801313770460" class="text-decoration-none text-dark">+8801313770460</a><br>
+            <a href="tel:+8809611611333" class="text-info text-decoration-underline">Whatsapp:</a><br>
+            <a href="tel:+8801313770460" class="text-decoration-none text-dark">+8801313770460</a><br>
+          </address>
         </div>
-
-        <!-- Contact Us Column -->
-        <div class="col-lg-3 col-md-6 mb-4 mb-lg-0">
-          <h4 class="mb-4">Contact Us</h4>
-          <p class="text-muted mb-2">hi@justhome.com</p>
-          <p class="text-muted mb-4">(123) 456-7890</p>
-          <h4 class="mb-3">Our Address</h4>
-          <p class="text-muted">99 Fifth Avenue, 3rd Floor<br>San Francisco, CA 1980</p>
+      </div>
+      <div class="row border-top pt-3 align-items-center bottom-footer">
+        <div class="col-md-8 small">
+          Copyright © 2025. Samaira Group
         </div>
-
-        <!-- Get the app Column -->
-        <div class="col-lg-3 col-md-6">
-          <h4 class="mb-4">Get the app</h4>
-          <div class="app-buttons">
-            <a href="#" class="store-link mb-3">
-              <img src="{{ asset('assets/frontassets/') }}/images/footer/link.png" alt="Download on Apple Store" class="img-fluid w-100">
-            </a>
-            <a href="#" class="store-link">
-              <img src="{{ asset('assets/frontassets/') }}/images/footer/link1.png" alt="Get it on Google Play" class="img-fluid w-100">
-            </a>
-          </div>
+        <div class="col-md-4 d-flex justify-content-md-end justify-content-center gap-3 mt-3 mt-md-0">
+          <a href="#" class="rounded-circle d-flex align-items-center justify-content-center" ><i class="fab fa-facebook-f"></i></a>
+          <a href="#" class="rounded-circle d-flex align-items-center justify-content-center" ><i class="fab fa-youtube"></i></a>
+          <a href="#" class="rounded-circle d-flex align-items-center justify-content-center" ><i class="fab fa-twitter"></i></a>
         </div>
       </div>
     </div>
   </footer>
-
-  <!-- Bottom Footer Section Start -->
-  <div class="bottom-footer py-3">
-    <div class="container">
-      <div class="text-left">
-        <p class="mb-0">Copyright © 2025. Samaira Group</p>
-      </div>
-    </div>
-  </div>
-  <!-- Bottom Footer Section End -->
+  <!-- Custom Footer Section End -->
 
   <!-- jQuery -->
   <script src="{{ asset('assets/frontassets/') }}/js/jquery-3.7.1.min.js"></script>
