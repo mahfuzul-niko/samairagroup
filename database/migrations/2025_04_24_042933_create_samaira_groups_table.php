5ba12633645d0d7db8e8cc7cd76efb8c3fadcf83
mahfuzulislam <mahfuzul1125@gmail.com>
2025-05-17 17:00:42 +0600

    done

--- database/migrations/2025_04_24_042933_create_samaira_groups_table.php
+++ database/migrations/2025_04_24_042933_create_samaira_groups_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,7 +12,7 @@ return new class extends Migration
     {
         Schema::create('samaira_groups', function (Blueprint $table) {
             $table->id();
-             
+            $table->integer('order')->default(0);
             $table->text('concern_image')->nullable();
             $table->text('concern_text')->nullable();
             $table->text('concern_link')->nullable();
