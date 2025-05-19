a88b14bd67260b12fa854ae7766ea8072d3fed4c
mahfuzulislam <mahfuzul1125@gmail.com>
2025-05-19 17:16:37 +0600

    done

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,6 +23,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('frontend.content.dashboard');
+        return view('frontend.content.redirect');
     }
 }
---
+++ resources/views/frontend/content/redirect.blade.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Redirect To Dashboard</title>
+    <link rel="icon" href="{{ asset('assets/frontassets/') }}/images/samaira-skills/fav-icon.svg" type="image/svg">
+
+    <!-- Bootstrap CSS -->
+    <link href="{{ asset('assets/frontassets/') }}/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Font Awesome CSS -->
+    <link rel="stylesheet" href="{{ asset('assets/frontassets/') }}/fonts/css/all.min.css">
+
+    <!-- Custom CSS -->
+    <link href="{{ asset('assets/frontassets/') }}/css/samaira-skills-register.css" rel="stylesheet">
+    <link href="{{ asset('assets/frontassets/') }}/css/samaira-main.css" rel="stylesheet">
+
+    <!-- Swiper CSS -->
+    <link rel="stylesheet" href="{{ asset('assets/frontassets/') }}/css/swiper-bundle.min.css" />
+
+    <!-- GLightbox CSS -->
+    <link rel="stylesheet" href="{{ asset('assets/frontassets/') }}/css/glightbox.min.css" />
+
+</head>
+
+<body>
+
+    <x-layouts.header />
+
+    <!-- Navbar Start -->
+
+    <x-layouts.navbar>
+        <x-slot name="logo">
+            <a href=""></a>
+        </x-slot>
+        <x-slot name="nav">
+            <li><a href="{{ route('page.home.about') }}">About Us</a></li>
+            <li><a href="{{ route('page.home.contact') }}">Contact Us</a></li>
+
+        </x-slot>
+    </x-layouts.navbar>
+    <!-- Navbar End -->
+
+    <!-- Register/Login Split Section Start -->
+    <section class="">
+
+    </section>
+    <!-- Register/Login Split Section End -->
+
+    <!-- Custom Footer Section Start -->
+    <x-layouts.footer />
+    <!-- Custom Footer Section End -->
+
+    <!-- jQuery -->
+    <script src="{{ asset('assets/frontassets/') }}/js/jquery-3.7.1.min.js"></script>
+    <!-- Swiper JS -->
+    <script src="{{ asset('assets/frontassets/') }}/js/swiper-bundle.min.js"></script>
+    <!-- Bootstrap JS -->
+    <script src="{{ asset('assets/frontassets/') }}/js/bootstrap.bundle.min.js"></script>
+    <!-- Popper JS -->
+    <script src="{{ asset('assets/frontassets/') }}/js/popper.min.js"></script>
+    <!-- Custom JS -->
+    <script src="{{ asset('assets/frontassets/') }}/js/samaira-new-index.js"></script>
+</body>
+
+</html>
