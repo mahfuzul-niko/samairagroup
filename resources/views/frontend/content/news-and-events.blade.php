5245601cb39f0270558fd7b5d3de7b874fcfd91c
mahfuzulislam <mahfuzul1125@gmail.com>
2025-05-31 17:25:56 +0600

    done

--- resources/views/frontend/content/news-and-events.blade.php
+++ resources/views/frontend/content/news-and-events.blade.php
@@ -31,27 +31,9 @@
 
     <!-- Navbar Start -->
     <x-layouts.navbar>
-        <x-slot name="logo">
-            <a href="#" class="rg-navbar-logo"><img
-                    src="{{ system_key('samaira_skills_logo') ? Storage::url(system_key('samaira_skills_logo')) : asset('assets/img/no-profile.png') }}" /></a>
-        </x-slot>
+        
         <x-slot name="nav">
-            <li><a href="{{ route('page.ssdi.about') }}">About Us</a></li>
-            <li><a href="{{ route('page.ssdi.contact') }}">Contact Us</a></li>
-
-
-
-            <li>
-                <div class="nav-auth">
-                    <div class="nav-item signup-btn">
-                        @auth
-                            <a href=""><i class="fa-solid fa-user"></i> <span>{{ auth()->user()->name }}</span></a>
-                        @else
-                            <a class="nav-link" href="{{ route('student.login') }}">Login</a>
-                        @endauth
-                    </div>
-                </div>
-            </li>
+          
         </x-slot>
     </x-layouts.navbar>
     <!-- Navbar End -->
